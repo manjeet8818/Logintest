0d6ea8214809bfeeaa0f4ef09c130e114014ce57
Manjeet Dahiya <manjeet@Manjeets-MacBook-Air.local>
2024-06-14 12:42:15 +1000

    first commit

--- api/src/controllers/todos/updateTodo.ts
+++ api/src/controllers/todos/updateTodo.ts
@@ -1,3 +1,31 @@
+// import { Context } from "hono";
+
+// const updateTodos = async (c: Context) => {
+//   const { todoStatus } = await c.req.json();
+//   const todoId = c.req.param("todoId");
+
+//   try {
+//     const { success } = await c.env.DB.prepare(
+//       "UPDATE todos SET is_completed = ? WHERE id = ?"
+//     )
+//       .bind(Number(todoStatus), todoId)
+//       .run();
+
+//     console.log("success", success);
+
+//     return c.json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     return c.json({
+//       success: false,
+//       message: "Error while updating  todos",
+//     });
+//   }
+// };
+
+// export default updateTodos;
+
 import { Context } from "hono";
 
 const updateTodos = async (c: Context) => {
@@ -13,14 +41,30 @@ const updateTodos = async (c: Context) => {
 
     console.log("success", success);
 
-    return c.json({
-      success: true,
-    });
+    return c.json(
+      {
+        success: true,
+      },
+      200,
+      {
+        "Access-Control-Allow-Origin": "https://logintest-1br.pages.dev",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PATCH",
+        "Access-Control-Allow-Headers": "Content-Type",
+      }
+    );
   } catch (error) {
-    return c.json({
-      success: false,
-      message: "Error while updating  todos",
-    });
+    return c.json(
+      {
+        success: false,
+        message: "Error while updating todos",
+      },
+      500,
+      {
+        "Access-Control-Allow-Origin": "https://logintest-1br.pages.dev",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PATCH",
+        "Access-Control-Allow-Headers": "Content-Type",
+      }
+    );
   }
 };
 
