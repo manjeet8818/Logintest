95f0ac8c420e8200a9f70b36006158acdf54cf85
Manjeet Dahiya <manjeet@Manjeets-MacBook-Air.local>
2024-06-14 15:59:58 +1000

    Try

--- _headers.js
+++
@@ -1,9 +0,0 @@
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://logintest-1br.pages.dev");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
