11f8d3cca1a85fe222b7007968d4c32c54899a2e
Manjeet Dahiya <manjeet@192-168-1-112.tpgi.com.au>
2024-06-14 18:48:44 +1000

    THis

--- api/src/index.ts
+++ api/src/index.ts
@@ -101,27 +101,27 @@ const app = new Hono<{ Bindings: Bindings }>();
 app.get("/", (c) => c.text("Hello Hono!"));
 
 // Global CORS Middleware
-// app.use(
-//   "/*",
-//   cors({
-//     origin: "https://logintest-1br.pages.dev", // Allow specific origin
-//     allowMethods: ["POST", "GET", "OPTIONS", "DELETE", "PATCH"],
-//     allowHeaders: ["Content-Type"],
-//   })
-// );
+app.use(
+  "/*",
+  cors({
+    origin: "*", // Allow specific origin
+    allowMethods: ["POST", "GET", "OPTIONS", "DELETE", "PATCH"],
+    allowHeaders: ["Content-Type"],
+  })
+);
 
-// Explicit CORS Headers Middleware
-app.use("*", async (c, next) => {
-  await next();
-  c.res.headers.append("Access-Control-Allow-Origin", "*");
-  c.res.headers.append(
-    "Access-Control-Allow-Methods",
-    "POST, GET, OPTIONS, DELETE, PATCH"
-  );
-  c.res.headers.append("Access-Control-Allow-Headers", "Content-Type");
-  console.log("Request Headers:", c.req.headers);
-  console.log("Response Headers:", c.res.headers);
-});
+// // Explicit CORS Headers Middleware
+// app.use("*", async (c, next) => {
+//   await next();
+//   c.res.headers.append("Access-Control-Allow-Origin", "*");
+//   c.res.headers.append(
+//     "Access-Control-Allow-Methods",
+//     "POST, GET, OPTIONS, DELETE, PATCH"
+//   );
+//   c.res.headers.append("Access-Control-Allow-Headers", "Content-Type");
+//   console.log("Request Headers:", c.req.headers);
+//   console.log("Response Headers:", c.res.headers);
+// });
 
 // Routes
 app.route("/todos", todosRoutes);
